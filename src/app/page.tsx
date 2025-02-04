660ebe1ee9f03f780ec664e7959f2143c93dde44
GEHAD001 <freelight665@gmail.com>
2025-02-04 19:12:00 +0200

    Fix Connector Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,13 +17,6 @@ export default function Home() {
 
 function SectionConnector() {
   return (
-    <div className="relative min-w-4 h-14 bg-gray-500/30 rounded-sm">
-      <div className="absolute -top-[30] -left-[0.6rem]">
-        <Hand className="size-8 stroke-[1px]" />
-      </div>
-      <div className="absolute -bottom-[30] -left-2  z-20 rotate-180">
-        <Grab className="size-8 stroke-[1px]" />
-      </div>
-    </div>
+    <div className="relative min-w-4 h-14 bg-gray-500/30 rounded-sm"></div>
   );
 }
